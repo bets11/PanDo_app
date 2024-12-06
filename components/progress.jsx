bee608fa16a35e64496a5edf39aa433694afe609
Betül <kulac.esrabetul@hotmail.com>
2024-12-06 13:47:33 +0100

    css changes in progress

--- components/progress.jsx
+++ components/progress.jsx
@@ -219,6 +219,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginVertical: 20,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 7,
+    elevation: 10,
   },
   progressPanda: {
     width: 300,
